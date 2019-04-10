29d09a5238329e72d8fbcacc2de431aa61726cd9
Hasan Aydoğdu <hsnaydd@gmail.com>
2019-04-10 11:28:54 +0300

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/index.tsx',
+    '!src/setupTests.ts',
+    '!src/components/**/index.{ts,tsx}',
+    '!src/components/**/*.stories.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
